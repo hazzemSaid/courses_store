59b8c352a3b3effd3b5a429fecbb3ed75fbc871a
haazemsaid <haazemsaidd@gmail.com>
2025-03-23 21:35:25 +0200

    add the cors to  enable different port from frontend

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-require('dotenv').config();
+var bodyParser = require('body-parser')
 var morgan = require("morgan");
+
+const cors= require("cors");
 const controller = require("./controller/courses_controller.js");
 const { body } = require("express-validator");
 const express = require("express");
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
 const app = express();
+require('dotenv').config();
+
+const url = process.env.DB_URL;
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // app.use(express.json());
-const url = process.env.DB_URL;
 mongoose.connect(url).then(() => {
    console.log("connected to the database");
 }).catch((err) => {
@@ -27,7 +32,7 @@ app.use("*",(req, res, next) => {
       message: "page not found",
    });
 })
-app.listen(3000, function () {
-   console.log("Server is running on port 3000");
+app.listen(process.env.PORT||3000, function () {
+   console.log("Server is ru nning on port", `${process.env.PORT}`);
 });
 
