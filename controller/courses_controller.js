28a53e53a715de21792e40eaf007b65883433ce1
haazemsaid <haazemsaidd@gmail.com>
2025-03-29 03:23:30 +0200

    add user model and integrate validator for email and phone number validation; update .gitignore to include node_modules

--- controller/courses_controller.js
+++ controller/courses_controller.js
@@ -4,79 +4,75 @@ const httpResponseText = require("../models/httpResponsetext");
 const asyncWrapper = require("../middleware/asyncwrapper");
 // ✅ Create a new course
 const Error_handler = require("../utils/error");
-const createNewCourse = asyncWrapper(
-	async (req, res,next) => {
-		// Validate request
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			
-			return res.status(400).json({ status: httpResponseText.Fail, error: errors.array() });
-		}
+const createNewCourse = asyncWrapper(async (req, res, next) => {
+	// Validate request
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res
+			.status(400)
+			.json({ status: httpResponseText.Fail, error: errors.array() });
+	}
 
-		// Create and save course
-		const newCourse = await Courses.create({ ...req.body });
-		// res.status(201).json(newCourse);
-		return res.status(201).json({
-			status: httpResponseText.Success,
-			data: newCourse,
-		})
-}
-);
+	// Create and save course
+	const newCourse = await Courses.create({ ...req.body });
+	// res.status(201).json(newCourse);
+	return res.status(201).json({
+		status: httpResponseText.Success,
+		data: newCourse,
+	});
+});
 
 // ✅ Get all courses
-const getAllCourses = asyncWrapper(
-	async (req, res) => {
-		const limit = req.query.limit || 10;
-		const page = req.query.page || 1;
-		const skip = (page - 1) * limit;
+const getAllCourses = asyncWrapper(async (req, res) => {
+	const limit = req.query.limit || 10;
+	const page = req.query.page || 1;
+	const skip = (page - 1) * limit;
 
-
-		const allCourses = await Courses.find({}, { "__v": false }).skip(skip).limit(limit);
-		// res.json(allCourses);
-		res.status(200).json({
-			status: httpResponseText.Success,
-			data: allCourses,
-		})
+	const allCourses = await Courses.find({}, { __v: false })
+		.skip(skip)
+		.limit(limit);
+	// res.json(allCourses);
+	res.status(200).json({
+		status: httpResponseText.Success,
+		data: allCourses,
 	});
+});
 
 // ✅ Get a single course by ID
-const getSingleCourse = asyncWrapper(
-	async (req, res, next) => {
-		const course = await Courses.findById(req.params.id, { "__v": false });
-		if (!course) {
-			const err =Error_handler("Course not found");
-			return next(err);
-			// return res.status(404).json({ stuts: httpResponseText.Fail, error:error.massage });
-			// return res.status(404).json({ error: "Course not found" });
-		}
-		res.json(course);
-
-	});
+const getSingleCourse = asyncWrapper(async (req, res, next) => {
+	const course = await Courses.findById(req.params.id, { __v: false });
+	if (!course) {
+		const err = Error_handler("Course not found");
+		return next(err);
+		// return res.status(404).json({ stuts: httpResponseText.Fail, error:error.massage });
+		// return res.status(404).json({ error: "Course not found" });
+	}
+	res.json(course);
+});
 
 // ✅ Update a course
 const updateCourse = asyncWrapper(async (req, res) => {
-	const course = await Courses.findById(req.params.id, { "__v": false });
+	const course = await Courses.findById(req.params.id, { __v: false });
 	if (!course) {
 		const err = Error_handler.createError("Course not found");
-	return 	next(err);
+		return next(err);
 	}
 	// Update values
 	course.title = req.body.title || course.title;
 	course.price = req.body.price || course.price;
 
 	// Save and return the updated course
-	await course.save({}, { "__v": false });
+	await course.save({}, { __v: false });
 	// res.json(course);
 	res.status(200).json({
 		status: httpResponseText.Success,
 		data: course,
-	}
-	)
+	});
 });
-const deleteCourse = asyncWrapper(
-	(req, res, next) => {
-		const id = req.params.id;
-		Courses.findByIdAndDelete(id, { "__v": false }).then((data) => {
+const deleteCourse = asyncWrapper((req, res, next) => {
+	const id = req.params.id;
+	Courses.findByIdAndDelete(id, { __v: false })
+		.then((data) => {
 			// res.json(data);
 			if (!data) {
 				const err = Error_handler.createError("Course not found");
@@ -85,17 +81,17 @@ const deleteCourse = asyncWrapper(
 			res.status(200).json({
 				status: httpResponseText.Success,
 				data: data,
-			})
-		}).catch((e) => {
+			});
+		})
+		.catch((e) => {
 			const err = Error_handler.createError(e.massage);
 			return next(err);
-		})
-	}
-)
+		});
+});
 module.exports = {
 	createNewCourse,
 	getAllCourses,
 	getSingleCourse,
 	updateCourse,
-	deleteCourse
+	deleteCourse,
 };
---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+
+const user = new mongoose.Schema({
+	id: {
+		type: mongoose.Schema.Types.ObjectId,
+		auto: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		validator: [validator.isEmail, 'Please enter a valid email']
+	},
+	phone: {
+		type: String,
+		required: true,
+		validator: [validator.isMobilePhone, 'Please enter a valid phone number']
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+module.exports = mongoose.model("User", user);
